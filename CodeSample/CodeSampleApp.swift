19a27c1b92d1e58f43ebb5566e526132077bd9ab
Kacper Jagiełło <kacperjagieo@gmail.com>
2024-08-14 11:41:59 +0200

    Initial commit.

---
+++ CodeSample/CodeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeSampleApp.swift
+//  CodeSample
+//
+//  Created by Kacper Jagiełło on 14/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CodeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodeSample
+//
+//  Created by Kacper Jagiełło on 14/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
